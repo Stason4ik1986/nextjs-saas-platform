df055327bac500f437d4f29b3521d87815b18318
Stanislav Cherednychenko <stanislav.cherednychenko@jelvix.com>
2025-06-22 13:50:54 +0300

    Implement Home Page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
 
-const Page = () => {
+import CTA from '@/components/CTA';
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+
+import { recentSessions } from '@/constants';
+
+const Home = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Saas App</h1>
-      <Button>Button</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Expolorer'
+          topic='Neural Network of the Brain'
+          subject='science'
+          duration={45}
+          color='#ffda6e'
+        />
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='maths'
+          duration={45}
+          color='#e5d0ff'
+        />
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature'
+          subject='language'
+          duration={30}
+          color='#bde7ff'
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+          title='Recently completed sessions'
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full'
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
-export default Page;
+export default Home;
